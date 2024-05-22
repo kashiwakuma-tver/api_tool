90e9ce9311c0dffe9e02db2b337ab934e6a96b85
kashiwakuma <kashiwakuma@n41406-MBP.local>
2024-05-22 11:32:43 +0900

    live追加

--- lib/api/tvercms/episode.rb
+++ lib/api/tvercms/episode.rb
@@ -9,7 +9,17 @@ class Episode
   end
 
   def self.episode_vod
-    api = ApiBase.new('episode_vod', 'epacfu64bq7')
+    api = ApiBase.new('episode_vod', 'id')
+    api.results_exec_api
+  end
+
+  def self.episode_lives
+    api = ApiBase.new('episode_live')
+    api.results_exec_paging_api
+  end
+
+  def self.episode_live
+    api = ApiBase.new('episode_live', 'id')
     api.results_exec_api
   end
 end
