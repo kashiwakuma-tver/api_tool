9b0d43ad2603ccf465445424263122d9242d4d9b
kashiwakuma <kashiwakuma@n41406-MBP.local>
2024-05-22 15:53:05 +0900

    不要なコード削除

--- lib/api/base.rb
+++ lib/api/base.rb
@@ -1,5 +1,5 @@
 module BASE
-  require 'active_support/all'
+  require 'active_support'
   require 'base64'
   require 'csv'
   require 'faraday'
@@ -8,11 +8,6 @@ module BASE
   require 'yaml'
 
   class << self
-    def args
-      args = { environment: ARGV[0], id: ARGV[1] }
-      # args unless args_check(args)
-    end
-
     def json_to_csv(class_name, jsons)
       timestamp = Time.now.strftime('%Y%m%d%H%M%S')
       file_name = "#{args[:environment]}_#{class_name}_#{timestamp}.csv"
@@ -36,16 +31,5 @@ module BASE
       end
       puts "#{file_name}を出力しました"
     end
-
-    private
-
-    # def args_check(args)
-    #   if args[:id]
-    #     unless (args[:id].start_with?(/ep|sr/) && args[:id].length == 10)
-    #       puts 'エピソードまたはシリーズのIDを正しくいれてください'
-    #       exit
-    #     end
-    #   end
-    # end
   end
 end
