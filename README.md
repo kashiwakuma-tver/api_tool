3dec91b96f9fe235850d3a1cdf50b5eb8ed902ef
kashiwakuma <kashiwakuma@n41406-MBP.local>
2024-06-04 11:53:54 +0900

    ドキュメント整備

---
+++ README.md
@@ -0,0 +1,33 @@
+# TVerCMS関連APIツール
+## このツールについて
+* このツールはTVerCMSのAPIを使ってCMSの情報を取得するためのツールである
+
+## 使い方
+1. main.rbを編集する。
+  1.実行したいAPIをコメントアウトする。例えばVODエピソード一覧APIを実行したい場合は「Episode.new~~.exec_paging_api」をコメントアウトする
+  2. optionsとparamsを指定する。大まかには以下の通りだが、詳細はmain.rbを参照のこと
+    1. options・・・実行するAPI、実行環境、IDなどを指定する
+    2. params・・・API実行時に指定するクエリパラメータ。必要に応じてAPIリファレンスの仕様に沿って指定する
+2. ターミナルでmain.rbのあるディレクトリに移動し「ruby main.rb」を実行する
+    
+
+## 対応API
+* TVerCMS
+  * VODエピソード一覧
+  * VODエピソード取得
+  * LIVEエピソード一覧
+  * LIVEエピソード取得
+  * シリーズ一覧
+  * シリーズ取得
+  * シーズン一覧
+  * シーズン取得
+  * VOD一覧
+* VideoCloud
+  * (準備中)
+* STREAKS
+  * (準備中)
+
+## APIリファレンス情報
+* [TVerCMS](https://s3-ap-northeast-1.amazonaws.com/tverservice-docs-omniscient/reference/cms/index.html#cms-episode-vod-get)
+* [STREAKS API](https://docs.streaks.jp/api/#/)
+* [VideoCloud API](https://apis.support.brightcove.com/index.html)
--- main.rb
+++ main.rb
@@ -5,18 +5,39 @@ require_relative 'lib/api/tvercms/season'
 require_relative 'lib/api/tvercms/vod'
 require_relative 'lib/api/contentmaster/contentmaster'
 
+# パラメータについて
+## options
+### api_type：実行するAPIを指定する
+### content_key：取得系のみ。一意のIDを指定する
+### environment：実行環境。dev、stg、prodのみ
+### broadcast_provider_id；ContentMasterのみ。取得したい局のIDを指定する
+## params
+### クエリパラメータを指定する。CMSリファレンスに沿って指定する
+
+#### episode系 ####
+# VODエピソード一覧
 # Episode.new({ api_type: 'episode/vod', content_key: nil, environment: 'dev' }).episode_vods
+
+# VODエピソード取得
 # Episode.new({ api_type: 'episode/vod', content_key: 'epljgx7ad7t', environment: 'dev' }).episode_vod
+
+# LIVEエピソード一覧
 # Episode.new({ api_type: 'episode/live', content_key: nil, environment: 'dev' }).episode_lives
+
+# LIVEエピソード取得
 # Episode.new({ api_type: 'episode/live', content_key: 'leyd4bt4dsv', environment: 'dev' }).episode_live
 
+#### series系 ####
 # Series.new({ api_type: 'series', content_key: nil, environment: 'dev' }).serieses
 # Series.new({ api_type: 'series', content_key: 'srm04h7ptuh', environment: 'dev' }).series
 
+#### season系 ####
 # Season.new({ api_type: 'series/sr30gynbcab/season', content_key: nil, environment: 'dev' }).series_seasons
 # Season.new({ api_type: 'series/sr30gynbcab/season', content_key: 'ssyzt9vf3k7', environment: 'dev' }).series_season
 # Season.new.season
 
+#### VOD管理系 ####
 # Vod.new.vod
 
-ContentMaster.new({ environment: 'dev', broadcast_provider_id: 'cx', api_type: 'series', content_key: nil }, { target: '1713315300' }).exec_api
+#### ContentMaster ####
+# ContentMaster.new({ environment: 'dev', broadcast_provider_id: 'cx', api_type: 'series', content_key: nil }, { target: '1713315300' }).exec_api
